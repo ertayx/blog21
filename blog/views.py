e28771e21d284fe07125ba28abde5a37484e9c76
Ertayx <ertaiesen05@gmail.com>
2022-07-26 12:55:26 +0600

    models + posts

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import PostSerializer, CommentSerializer, LikeSerializer
+from .models import Post
+
+@api_view(['GET'])
+def posts_list(request):
+    queryset = Post.objects.all()
+    serialiser = PostSerializer(queryset, many = True)
+    return Response(serialiser.data)
+
